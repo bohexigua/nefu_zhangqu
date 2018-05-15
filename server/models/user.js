cf2649d4e6dc24565a87334a778bf83a7316b254
bohexigua <aa869410917bb@qq.com>
2018-05-15 19:50:58 +0800

    fixed

--- server/models/user.js
+++ server/models/user.js
@@ -20,7 +20,7 @@ const getUserInfo = async (username, password) => {
   await client.startTransaction();
   const sql1 = 'insert into user(user_name) select user_no from campus_info where not exists(select * from user where user_name = ?) and user_no = ?;';
   await client.executeTransaction(sql1, [username, username]);
-  const sql2 = 'select c.user_no, c.user_name, c.user_sex, c.user_college, c.user_img, ui.user_phone, ui.user_mail from user u, campus_info c, user_info ui where u.user_name = ? and u.user_pwd = ? and c.user_no = ? and ui.user_no = c.user_no';
+  const sql2 = 'select c.user_no, c.user_name, c.user_sex, c.user_college, c.user_img, ui.user_phone, ui.user_mail from user u, campus_info c left join user_info ui on ui.user_no = c.user_no where u.user_name = ? and u.user_pwd = ? and c.user_no = ?';
   const res = await client.executeTransaction(sql2, [username, password, username]);
   await client.stopTransaction();
   return res;
